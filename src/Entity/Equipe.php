c4ac8e064693d9c46511d47482d7d0927fef85cf
sourourbelghith <sourour.belghith123@gmail.com>
2022-04-22 20:13:09 +0200

    crud

--- src/Entity/Equipe.php
+++ src/Entity/Equipe.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Equipe
  *
- * @ORM\Table(name="equipe", indexes={@ORM\Index(name="equipe_personne", columns={"id_coach"})})
+ * @ORM\Table(name="equipe")
  * @ORM\Entity
  */
 class Equipe
@@ -24,26 +24,33 @@ class Equipe
     /**
      * @var string
      *
-     * @ORM\Column(name="nom_eq", type="string", length=255, nullable=false)
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
      */
-    private $nomEq;
+    private $nom;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description_equipe", type="string", length=255, nullable=false)
+    public function getIdEquipe(): ?int
+    {
+        return $this->idEquipe;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+  /**
+     * @return string
      */
-    private $descriptionEquipe;
-
-    /**
-     * @var \Personne
-     *
-     * @ORM\ManyToOne(targetEntity="Personne")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="id_coach", referencedColumnName="id_personne")
-     * })
-     */
-    private $idCoach;
-
-
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return $this->nom;
+    }
 }
--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Personne
  *
- * @ORM\Table(name="personne", indexes={@ORM\Index(name="personne_equipe", columns={"id_equipe"})})
+ * @ORM\Table(name="personne")
  * @ORM\Entity
  */
 class Personne
@@ -22,119 +22,75 @@ class Personne
     private $idPersonne;
 
     /**
-     * @var string|null
+     * @var string
      *
-     * @ORM\Column(name="nom_personne", type="string", length=20, nullable=true)
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
      */
-    private $nomPersonne;
+    private $nom;
 
     /**
-     * @var string|null
+     * @var string
      *
-     * @ORM\Column(name="prenom_personne", type="string", length=30, nullable=true)
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=false)
      */
-    private $prenomPersonne;
+    private $prenom;
 
     /**
-     * @var int|null
+     * @var int
      *
-     * @ORM\Column(name="contact", type="integer", nullable=true)
+     * @ORM\Column(name="age", type="integer", nullable=false)
      */
-    private $contact;
+    private $age;
+
+    public function getIdPersonne(): ?int
+    {
+        return $this->idPersonne;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
 
     /**
-     * @var int|null
-     *
-     * @ORM\Column(name="rating", type="integer", nullable=true)
+     * @return string
      */
-    private $rating;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="role", type="string", length=30, nullable=true)
-     */
-    private $role;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="mot_de_passe", type="string", length=15, nullable=true)
-     */
-    private $motDePasse;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="email", type="string", length=50, nullable=true)
-     */
-    private $email;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="description", type="string", length=500, nullable=true)
-     */
-    private $description;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="competence", type="string", length=250, nullable=true)
-     */
-    private $competence;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="jeux", type="string", length=50, nullable=true)
-     */
-    private $jeux;
-
-    /**
-     * @var float|null
-     *
-     * @ORM\Column(name="prix", type="float", precision=10, scale=0, nullable=true)
-     */
-    private $prix;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="heros", type="string", length=20, nullable=true)
-     */
-    private $heros;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="ig_name", type="string", length=20, nullable=true)
-     */
-    private $igName;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="ig_role", type="string", length=10, nullable=true)
-     */
-    private $igRole;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="ig_rank", type="integer", nullable=true)
-     */
-    private $igRank;
-
-    /**
-     * @var \Equipe
-     *
-     * @ORM\ManyToOne(targetEntity="Equipe")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="id_equipe", referencedColumnName="id_equipe")
-     * })
-     */
-    private $idEquipe;
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return $this->nom;
+    }
 
 
 }
