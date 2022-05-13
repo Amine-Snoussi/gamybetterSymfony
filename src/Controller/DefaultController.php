b8c2980143b1aa6bc6ba105ab2f7fdb69a668cee
SkanderGassa <skander.gassa@esprit.tn>
2022-05-13 05:17:40 +0100

    Merged Sourour

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -36,16 +36,7 @@ class DefaultController extends AbstractController
             
         ]);
     }
-    /**
-     * @Route("/teammate", name="teammate")
-     * @return Response
-     */
-    public function profile(): Response
-    {
-        return $this->render('profile.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
+
     /**
      * @Route("/blog", name="blog")
      * @return Response
@@ -106,56 +97,7 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
-    /**
-     * @Route("/blog", name="blog")
-     * @return Response
-     */
-    public function blog(): Response
-    {
-        return $this->render('blog-grid.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-    /**
-     * @Route("/gallery", name="galery")
-     * @return Response
-     */
-    public function gallery(): Response
-    {
-        return $this->render('gallery.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-    /**
-     * @Route("/store", name="store")
-     * @return Response
-     */
-    public function store(): Response
-    {
-        return $this->render('store.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-    /**
-     * @Route("/product", name="product")
-     * @return Response
-     */
-    public function product(): Response
-    {
-        return $this->render('product.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-    /**
-     * @Route("/admin", name="admin")
-     * @return Response
-     */
-    public function admin(): Response
-    {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
+
     /**
      * @Route("/watch", name="watch")
      * @return Response
